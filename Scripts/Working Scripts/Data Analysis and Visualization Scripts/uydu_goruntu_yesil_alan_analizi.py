80f9887b18db2668159c5facdbff5ed28c38a610
sukruburakcetin <cetin.sukruburak@gmail.com>
2021-08-17 21:05:28 +0300

    koroplet harita, görüntü işleme algoritması cıktılarında kullanılarak görüntü anlaşılır hâle getirildi.

--- Scripts/Working Scripts/Data Analysis and Visualization Scripts/uydu_goruntu_yesil_alan_analizi.py
+++ Scripts/Working Scripts/Data Analysis and Visualization Scripts/uydu_goruntu_yesil_alan_analizi.py
@@ -3,8 +3,12 @@ import os
 
 import cv2
 import numpy as np
+import pandas as pd  # For general data processing tasks
 from matplotlib import pyplot as plt
-
+import geopandas as gpd  # A module built on top of pandas for geospatial analysis
+import contextily as ctx  # Used in conjuction with matplotlib/geopandas to set a basemap
+import matplotlib.cm as cm
+import matplotlib.colors as col
 
 def auto_canny(image, sigma=0.33):
     # compute the median of the single channel pixel intensities
@@ -17,7 +21,52 @@ def auto_canny(image, sigma=0.33):
 
     # return the edged image
     return edged
+# %% --- Helper functions and definitions ---
 
+# Helper function: Add labels to the top of a bar chart.
+def add_value_labels(ax, spacing=5):
+    """Add labels to the end of each bar in a bar chart.
+
+    Arguments:
+        ax (matplotlib.axes.Axes): The matplotlib object containing the axes
+            of the plot to annotate.
+        spacing (int): The distance between the labels and the bars.
+    """
+
+    # For each bar: Place a label
+    for rect in ax.patches:
+        # Get X and Y placement of label from rect.
+        y_value = rect.get_height()
+        x_value = rect.get_x() + rect.get_width() / 2
+
+        # Number of points between bar and label. Change to your liking.
+        space = spacing
+        # Vertical alignment for positive values
+        va = 'bottom'
+
+        # If value of bar is negative: Place label below bar
+        if y_value < 0:
+            # Invert space to place label below
+            space *= -1
+            # Vertically align label at top
+            va = 'top'
+
+        # Use Y value as label and format number with one decimal place
+        label = "{:}".format(y_value)  # Remove .1f if you don't want one decimal place
+
+        # Create annotation
+        ax.annotate(
+            label,  # Use `label` as label
+            (x_value, y_value),  # Place label at end of the bar
+            xytext=(0, space),  # Vertically shift label by `space`
+            textcoords="offset points",  # Interpret `xytext` as offset in points
+            ha='center',  # Horizontally center label
+            va=va,  # Vertically align label differently for  positive and negative values.
+            rotation=90,
+            fontsize=15)  # Rotate label
+
+
+# Helper definitions --- Set dictionaries for fonts
 
 # %% --- Dynamically create a directory named after the file for outputs ---
 
@@ -53,31 +102,59 @@ font_axislabels = {'family': 'sans-serif',
                    'weight': 'bold',
                    'size': 10}
 
+font_yticks = {'family': 'sans-serif',
+               "fontname": "Arial",
+               'color': 'black',
+               'weight': 'normal',
+               'size': 16}
+
+font_xticks = {'family': 'sans-serif',
+               "fontname": "Arial",
+               'color': 'black',
+               'weight': 'bold',
+               'size': 16}
+
 complete_output_directory = incomplete_output_directory + filename_final_processed
 kernel = np.ones((3, 3), np.uint8)
-image = cv2.imread('../../../Data/Non-GIS Data/external/satellite-map-of-istanbul.jpg')
-image_2 = cv2.imread('../../../Data/Non-GIS Data/external/yayilim_with_no_name_tr.jpg', 0)
 
-image_clone = image.copy()
+# Istanbul health services data
+greenIndex_fp = "../../../Data/Non-GIS Data/external/district_income.xlsx"
+greenIndex = pd.read_excel(greenIndex_fp)
 
-h, w = image_2.shape
+# Istanbul geospatial districts data
+istanbul_districts_fp = "../../../Data/GIS data/Processed/istanbul_districts.shp"
+istanbul_districts = gpd.read_file(istanbul_districts_fp, )
+
+satellite_2D_istanbul = cv2.imread('../../../Data/Non-GIS Data/external/satellite-map-of-istanbul.jpg')
+raw_shapefile_districts = cv2.imread('../../../Data/Non-GIS Data/external/yayilim_with_no_name_tr.jpg', 0)
+raw_shapefile_districts_cleaned = cv2.imread('../../../Data/Non-GIS Data/external/districts_cleaned.jpg', 0)
+
+# Istanbul geospatial districts data
+istanbul_districts_fp = "../../../Data/GIS data/Processed/istanbul_districts.shp"
+istanbul_districts = gpd.read_file(istanbul_districts_fp, )
+
+image_clone = satellite_2D_istanbul.copy()
+
+h, w = raw_shapefile_districts.shape
 size = h, w, 1
 empty_mat = np.zeros(size, dtype=np.uint8)
 
-blur = cv2.GaussianBlur(image_2, (1, 1), 0)
+blur = cv2.GaussianBlur(raw_shapefile_districts, (1, 1), 0)
 edges = auto_canny(blur)
-thresh = cv2.threshold(edges, 3, 255, cv2.ADAPTIVE_THRESH_MEAN_C | cv2.THRESH_OTSU)[1]
-edges = cv2.Canny(image_2, threshold1=100, threshold2=200)
+
+edges = cv2.Canny(raw_shapefile_districts, threshold1=100, threshold2=200)
 edges_copy_alt = edges.copy()
 closing = cv2.morphologyEx(edges_copy_alt, cv2.MORPH_CLOSE, kernel)
-contours, hierarchy = cv2.findContours(closing, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
+
+contours, hierarchy = cv2.findContours(raw_shapefile_districts_cleaned, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
+
 font = cv2.FONT_HERSHEY_SIMPLEX
-h, w, _ = image.shape
+h, w, _ = satellite_2D_istanbul.shape
 mask = np.zeros((h + 2, w + 2), np.uint8)
 counter = 0
 greenRate = -1
 minContourArea = 10
-image_converted = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+image_converted = cv2.cvtColor(satellite_2D_istanbul, cv2.COLOR_BGR2RGB)
 
 for c in contours:
     contourArea = cv2.contourArea(c)
@@ -120,8 +197,8 @@ for c in contours:
             greenRate = 1
         elif 225 <= gray_avg < 256:
             greenRate = 0
-        cv2.putText(image_converted, str(greenRate), (cX, cY), font, .5, (0), 2)
-        cv2.putText(image, str(greenRate), (cX, cY), font, .5, (0), 2)
+        cv2.putText(image_converted, str(greenRate), (cX, cY), font, .3, (0), 1)
+        cv2.putText(satellite_2D_istanbul, str(greenRate), (cX, cY), font, .3, (0), 1)
         freqs = np.cumsum(np.hstack([[0], counts[indices] / float(counts.sum())]))
         rows = np.int_(ROI.shape[0] * freqs)
         dom_patch = np.zeros(shape=ROI.shape, dtype=np.uint8)
@@ -132,24 +209,173 @@ for c in contours:
         counter += 1
 
 cv2.drawContours(image_converted, contours, -1, (0, 0, 255), 0)
-cv2.drawContours(image, contours, -1, (0, 0, 255), 0)
-cv2.imwrite(complete_output_directory + r"/" + filename_final_processed + "contoursOverlayed.png", image)
-f, (ax0, ax1) = plt.subplots(1, 2)
-ax0.imshow(image_converted, interpolation='nearest')
-ax1.imshow(closing, interpolation='nearest')
-ax0.set_title("Uydu görüntüsü üzerinden yeşil alan miktarına göre puanlama",
-               color="black",
-               weight="bold",
-               fontsize=5,
-               pad=5)
+cv2.drawContours(satellite_2D_istanbul, contours, -1, (0, 0, 255), 0)
+cv2.imwrite(complete_output_directory + r"/" + filename_final_processed + "contoursOverlayed.png",
+            satellite_2D_istanbul)
 
-ax0.set_xlabel("Katsayı",
+green_area_index_district = greenIndex.loc[:, ['district_eng', 'green_area_index']]
+
+green_area_index_district.rename(columns={"district_eng": "district_e", "green_area_index": "green_area_index"},
+                                 inplace=True)
+
+istanbul_districts = istanbul_districts.merge(green_area_index_district,
+                                              on="district_e",
+                                              how="left")
+
+# Now, this information can be used for both TR and eng
+
+# %% --- Visualization - English ---
+
+# --- Figure Preparation ---
+
+fig = plt.figure(figsize=(19.20, 19.20))
+
+ax_1 = fig.add_subplot(2, 1, 1)
+
+#                            --- MAP: ---
+
+# --- Plot Figure ---
+
+istanbul_districts.plot(ax=ax_1,
+                        column="green_area_index",
+                        edgecolor="black",
+                        alpha=1,
+                        cmap=cm.BuGn)
+# YlGnBu -> blue schema
+# --- Set Basemap ---
+
+ctx.add_basemap(ax_1, zoom=11,  # 16
+                crs='epsg:4326',
+                source=ctx.providers.Esri.WorldGrayCanvas)
+
+# --- Spine and Grid ---
+
+ax_1.set_axis_off()  # Turn off axis
+
+# --- Map Labels ---
+
+# Select districts that you want labels for
+districts_to_label_list = ["Silivri", "Catalca", "Buyukcekmece", "Arnavutkoy", "Eyupsultan", "Sariyer",
+                           "Beykoz", "Sile", "Cekmekoy", "Tuzla", "Pendik", "Maltepe", "Basaksehir", "Bahcelievler",
+                           "Beyoglu"]
+
+districts_to_label_indexes = [31, 5, 16, 3, 20, 33, 13, 1, 17, 37, 30]
+
+# Create a boolean indexing mask checking for those districts
+labels_mask = istanbul_districts.loc[:, "district_e"].isin(districts_to_label_list)
+
+# Pass in the boolean mask to create a dataframe
+districts_to_label = istanbul_districts.loc[labels_mask, ["district_e", "geometry"]]
+
+# Create a representative point within each district polygon to place the label
+districts_to_label["representative_point"] = districts_to_label.geometry.representative_point().geometry.values
+
+# Pass over each row label the repsentative point according to that row's name
+for idx, row in districts_to_label.iterrows():
+    ax_1.annotate(s=row["district_e"], xy=(row["representative_point"].x, row["representative_point"].y),
+                  horizontalalignment='center')
+
+#                           --- BAR CHART: ---
+
+# --- Figure Preparation ---
+ax_2 = fig.add_subplot(2, 1, 2)
+
+# --- Data Selection ---
+
+# Get labels for x - axis ticks
+labels = list(green_area_index_district.loc[:, "district_e"].value_counts().sort_values(ascending=False).index)
+
+# Generate bar positions
+from numpy import arange
+
+bar_positions = arange(len(labels)) + 1
+
+# Get bar heights from data
+bar_heights = green_area_index_district.loc[:, "green_area_index"].values.astype(int)
+
+# --- Color Information ---
+
+# For health data  when both graphs and maps are used.
+sequential_cmap = cm.ScalarMappable(col.Normalize(0, max(bar_heights)), cm.BuGn)
+
+# --- Plot Figure ---
+
+ax_2.bar(bar_positions, bar_heights,
+         width=0.7,
+         align="center",
+         color=sequential_cmap.to_rgba(bar_heights))
+
+# --- Add color legend ---
+
+# Import required toolkit
+from mpl_toolkits.axes_grid1.inset_locator import inset_axes
+
+# Create an inset_axes instance with different parameters
+axins1 = inset_axes(ax_2,
+                    width="15%",  # width = 50% of parent_bbox width
+                    height="3%",  # height : 5%
+                    loc='upper right')
+
+# Create a colormap
+cbar = plt.colorbar(sequential_cmap,
+                    cax=axins1,
+                    # ticks = [] Cna also set ticks like this
+                    orientation="horizontal",
+                    shrink=0.25,
+                    anchor=(30, 10))
+
+cbar.set_label('Yeşil Alan Index Skoru',
+               size=14,
+               weight="bold")
+
+# --- Set x and y axis ticks ---
+
+# Setting where x-ticks should be at
+ax_2.set_xticks(bar_positions)
+
+# Setting x-tick labels and positions
+ax_2.set_xticklabels(labels, rotation=90)
+
+# Setting custom y-axis tick intervals
+start, end = ax_2.get_ylim()
+ax_2.yaxis.set_ticks(np.arange(start, end, 50))
+
+# --- Spine and Grid ---
+
+# Disable right and top spine
+ax_2.spines['right'].set_visible(False)
+ax_2.spines['top'].set_visible(False)
+
+# --- Text ---
+
+# Add x axis label
+ax_2.set_xlabel("District",
                 fontdict=font_axislabels,
                 labelpad=18)
-#plt.show()
-export_path = complete_output_directory + r"/" + (filename_final_processed + "_figure.svg")
+
+# Add y axis label
+ax_2.set_ylabel("Number of parks/green areas",
+                fontdict=font_axislabels,
+                labelpad=18)
+
+# Add labels to the top of the bars
+add_value_labels(ax_2)
+
+# Set xtick font info
+
+ax_2.set_xticklabels(ax_2.get_xticklabels(),
+                     font_xticks)
+
+# --- Misc ---
+
+# Remove the empty white-space around the axes
+plt.tight_layout()
+
+# -------------
+# plt.show()
+export_path = complete_output_directory + r"/" + (filename_final_processed + "_figur_koroplet_ve_bar.svg")
 plt.savefig(export_path, format="svg", dpi=900, bbox_inches="tight")
 
 # As png
-export_path = complete_output_directory + r"/" + (filename_final_processed + "_figure.png")
+export_path = complete_output_directory + r"/" + (filename_final_processed + "_figur_koroplet_ve_bar.png")
 plt.savefig(export_path, format="png", dpi=300, bbox_inches="tight")
